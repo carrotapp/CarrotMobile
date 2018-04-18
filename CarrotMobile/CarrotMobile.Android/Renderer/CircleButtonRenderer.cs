0c5e7722099cc37c72a684329b385e53db192ca4
MC-Tortuga <malcolmcarelse@gmail.com>
2018-04-18 12:22:40 +0200

    Empty dashboard state *Custom Renderer for circle button

---
+++ CarrotMobile/CarrotMobile.Android/Renderer/CircleButtonRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+using CarrotMobile;
+using CarrotMobile.Droid.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CircleButton), typeof(CircleButtonRenderer))]
+
+namespace CarrotMobile.Droid.Renderer
+{
+    internal class CircleButtonRenderer : ButtonRenderer
+    {
+        public CircleButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.LightGreen);
+            }
+        }
+    }
+}
